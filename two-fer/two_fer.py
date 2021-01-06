a2698e60ee1f4cc33ff6675da4da844cb24b7402
Preshh0 <uniquep201@gmail.com>
2021-01-06 15:33:19 +0100

    I'm tryin to correct my corrections. I still have errors though.

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,11 +1,28 @@
 
+# def two_fer():
+
+#     if two_fer != "":
+#         print ("One for " + two_fer + " one for me." )
+#     elif two_fer == str() :
+#         print ("One for you, one for me.")
+#     else:
+#         raise Exception("Meaningful message indicating the source of the error")
+
+# two_fer("name")
+
+
 def two_fer():
-    name = "Jane"
-    if name == "Jane":
-        print ("One for %r, one for me." %name)
-    elif name == "" :
+    name = input("Please input name: ")
+
+    if name == True:
+        print ("One for " + name + ", one for you.")
+    elif name == "":
         print ("One for you, one for me.")
     else:
         raise Exception("Meaningful message indicating the source of the error")
 
+
 two_fer()
+
+# output = input("please input name: ")
+# print(output)
