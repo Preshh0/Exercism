f812f38b317026d193c59868329a2751e0c86757
Preshh0 <uniquep201@gmail.com>
2021-01-07 17:32:13 +0100

    Just a bunch of other incorrect trials. I need help

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -12,17 +12,46 @@
 
 
 def two_fer():
-    # name = input("Please input name: ")
+    name = input("Please input name: ")
 
-    if two_fer() == True:
-        print ("One for " + two_fer() + ", one for you.")
-    elif two_fer() == "":
+    if name == True:
+        print ("One for " + name + ", one for you.")
+    elif name == "":
         print ("One for you, one for me.")
     else:
         raise Exception("Supposed to print out hello greetings.")
 
 
 two_fer()
+'''
+#This one's wrong too.
 
+def name():
+    user_input = input("What is your name?")
+    print("One for " + user_input + ", one for you.")
+
+def two_fer():
+    print("One for you, one for me.")
+
+if name() == True:
+    print(name())
+elif name() == False:
+    print(two_fer())
+'''
+
+'''
+#This too. Omo.
+def name():
+    user_input = input("What is your name?")
+    print("One for " + name + ", one for me.")
+
+def two_fer():
+    if str(name()) == True:
+        print(name())
+    else:
+        print("One for you, one for me.")
+two_fer()
+
+'''
 # output = input("please input name: ")
 # print(output)
