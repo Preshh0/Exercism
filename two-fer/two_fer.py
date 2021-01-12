60898087244a562e91ac6435163cda02f0d2d707
Preshh0 <uniquep201@gmail.com>
2021-01-12 12:55:53 +0100

    The actual correct code. The if statement was unnecessary. A W!

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,10 +1,7 @@
 
 def two_fer(name = "you"):
 
-    if name != "":
-        return(f"One for {name}, one for me.")
-    else:
-        return(f"One for {name}, one for me.")
+    return(f"One for {name}, one for me.")
 
 print(two_fer("Alice"))
 print(two_fer("Bob"))
